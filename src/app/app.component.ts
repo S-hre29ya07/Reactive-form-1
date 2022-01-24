1881624869c6cc0b3b66649bdcdc68434036e9b9
S-hre29ya07 <singh.shreya2907@gmail.com>
2022-01-24 14:54:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { FormGroup,FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Forms';
+  title = 'Angular Reactive Form';
+  loginForm= new FormGroup({
+    user:new FormControl('',[Validators.required,Validators.email]),
+    password:new FormControl('',[Validators.required,Validators.minLength(5)]),
+
+  })
+  loginUser(){
+    console.warn(this.loginForm.value)
+  }
+
+  get user()
+  {
+    return this.loginForm.get('user');
+  }
+  get password()
+  {
+    return this.loginForm.get('password');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatButtonModule} from '@angular/material/button';
 
+  const MaterialComponents = [
+    MatButtonModule
+];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,ReactiveFormsModule,
+    MaterialComponents,
   ],
   providers: [],
   bootstrap: [AppComponent]
